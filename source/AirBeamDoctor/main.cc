bf96ef3ff6c633da63fee165b9425672002dc835
ChenKS12138 <chenks12138@gmail.com>
2025-05-25 21:50:20 +0800

    feat: add AirBeamDoctor

--- source/AirBeamDoctor/main.cc
+++ source/AirBeamDoctor/main.cc
@@ -1,6 +1,44 @@
+#include <fstream>
+#include <ios>
 #include <iostream>
+#include <thread>
+
+#include "raop/raop.h"
+
+using namespace AirBeamCore::raop;
 
 int main() {
-  std::cout << "Hello, World!" << std::endl;
+  std::cout << "Begin App" << std::endl;
+
+  Raop raop("192.168.123.109");
+  raop.Start();
+
+  std::ifstream ifs(
+      "/Users/cattchen/Codes/github.com/ChenKS12138/AirBeam/images/audio.pcm",
+      std::ios::binary);
+
+  if (ifs.is_open()) {
+    ifs.seekg(0, std::ios::end);
+    size_t length = ifs.tellg();
+    ifs.seekg(0, std::ios::beg);
+
+    char* buffer = new char[length];
+
+    std::cout << "Reading " << length << " characters... " << std::endl;
+    ifs.read(buffer, length);
+
+    if (ifs) {
+      std::cout << "all characters read successfully." << std::endl;
+    } else {
+      std::cout << "error: only " << ifs.gcount() << " could be read";
+    }
+    ifs.close();
+
+    // TODO: Send buffer to raop
+    delete[] buffer;
+  } else {
+    std::cout << "Unable to open file";
+  }
+
   return 0;
 }
